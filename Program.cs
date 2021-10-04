a3dcff5204ceee275dc32cb4a5c6cafd4eacf753
Brenin Peterson <etharas130@byui.edu>
2021-10-04 13:08:20 -0600

    part 2 stuff

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace team3v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
